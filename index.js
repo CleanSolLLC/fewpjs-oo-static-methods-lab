6506d6f760df04e3a013cc11d73f61562f7c8b55
mamark90 <mamark90@gmail.com>
2021-09-23 11:46:05 -0400

    commit tree cc5a1bda25570abe38aa99f55c44d96d8d0b910a

--- index.js
+++ index.js
@@ -1,3 +1,24 @@
 class Formatter {
-  //add static methods here
+  static capitalize(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1)
+  }
+
+  static sanitize(string) {
+    return string.replace(/[^A-Za-z0-9-'" "]+/g, '');
+  }
+
+  static titleize(string) {
+    const arry = ['the', 'a', 'an', 'but', 'of', 'and', 'for', 'at', 'by', 'from']
+    //console.log(string.split(" ").map(word => this.capitalize(word)).join(" "))
+
+    let newString = string.split(" ").map(word => {
+      if (!arry.includes(word) || string[0] === word){
+        return this.capitalize(word)
+      } else {
+        return word
+      }
+      })
+    //console.log(newString)
+    return newString.join(" ")
+    }
 }
